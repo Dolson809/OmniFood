f6ceffb64e56923a37f5258753afd9382a676d4a
Daniel Olson <sanjose1994@gmail.com>
2020-05-15 13:39:35 -0700

    Added mobile nav and reduced img size

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -10,4 +10,71 @@ $(document).ready(function(){
         }, {
           offset: '60px;'
         });
-})
+
+        /* Scroll on button */
+        $('.js--scroll-to-plans').click(function(){
+            $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
+        });
+
+                /* Scroll on top */
+                $('.js--scroll-to-start').click(function(){
+                    $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+                });
+
+            /* Navigation scroll */
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
+    /* Animation on scroll */
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        nav.slideToggle(200);
+        if (icon.hasClass('fa-bars')) {
+            icon.addClass('fa-times');
+            icon.removeClass('fa-bars');
+        } else {
+            icon.addClass('fa-bars');
+            icon.removeClass('fa-times');
+        }
+
+
+    });
+});
